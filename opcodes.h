a2028cb8bd81730b7dc5b99e854c072f52d24bca
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2024-03-19 12:46:05 +0000

    parse addressing mode

--- opcodes.h
+++ opcodes.h
@@ -1,15 +1,30 @@
 #pragma once
 
-constexpr int S = 2;
+enum {
+    S = 2,
+    M = 3
+};
 
-enum struct ESize {
+enum struct ESize : u8 {
     Byte,
     Word,
     Long
 };
 
+enum struct EMode : u8 {
+    Dn,
+    An,
+    Addr,
+    AddrInc,
+    AddrDec,
+    AddrDisp,
+    AddrIdx,
+    Other
+};
+
 struct TInstruction {
     ESize size;
+    EMode mode;
 };
 
 template<size_t Size>
@@ -35,18 +50,27 @@ bool Process(const std::array<int, Size>& mask, u16 opcode, std::function<void(c
             }
             return false;
 
-        case S:
-            if (bitset[i] == 0 && bitset[i + 1] == 0) {
-                instruction.size = ESize::Byte;
-            } else if (bitset[i] == 0 && bitset[i + 1] == 1) {
-                instruction.size = ESize::Word;
-            } else if (bitset[i] == 1 && bitset[i + 1] == 0) {
-                instruction.size = ESize::Long;
+        case S: {
+            u8 size = (bitset[i] << 1) | bitset[i + 1];
+            if (size < 3) {
+                instruction.size = ESize{size};
             } else {
                 return false;
             }
             i += 2;
             continue;
+        }
+
+        case M: {
+            u8 mode = (bitset[i] << 2) | (bitset[i + 1] << 1) | bitset[i + 2];
+            if (mode < 8) {
+                instruction.mode = EMode{mode};
+            } else {
+                return false;
+            }
+            i += 3;
+            continue;
+        }
 
         default:
             throw std::runtime_error("Unknown opcode mask.");
